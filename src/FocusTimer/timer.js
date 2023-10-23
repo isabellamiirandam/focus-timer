db55864d14ed2e64297d66d4b7edf77aac475bf5
Isabella Miranda <isabellamiirandam@gmail.com>
2023-10-23 14:38:32 -0300

    atualizando display do contador

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -4,4 +4,7 @@ import * as el from './elements.js'
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes
   seconds = seconds ?? state.seconds
+
+  el.minutes.textContent = String(minutes).padStart(2, "0")
+  el.seconds.textContent = String(seconds).padStart(2, "0")
 }
