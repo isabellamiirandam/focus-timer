a788b11441147309ba3ead6f361990e63381b74a
Isabella Miranda <isabellamiirandam@gmail.com>
2023-10-23 10:12:42 -0300

    refatorando lógica

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -4,9 +4,10 @@ import * as actions from "./actions.js"
 export function registerControls() {
   controls.addEventListener('click', (event) => {
     const action = event.target.dataset.action
-    if (action === undefined) {
+    if (typeof actions[action] != 'function') {
       return
     }
-    console.log(action)
+
+    actions[action]()
   })
 }
